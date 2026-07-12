d321715bd6c12815187201f167cc47b0e27148c3
Ayesha-Saddique9 <142135634+Ayesha-Saddique9@users.noreply.github.com>
2026-07-12 21:08:38 +0500

    fixed homepage + vite base for deployment

--- admin-panel/src/main.jsx
+++ admin-panel/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 )
